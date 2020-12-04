344edca4f142985566319a896f90f29e68805539
Shaun Sharples <shaun.sharples@gmail.com>
2020-12-04 15:37:10 +0200

    comments and make license hard link to fix nix-build

--- day1/Main.hs
+++ day1/Main.hs
@@ -5,17 +5,24 @@ import qualified Data.IntSet as IS
 import Data.List (lines)
 import System.IO (hGetContents, stdin)
 
+-- Read input on stdin as a set of integers
 input :: IO IS.IntSet
 input = IS.fromList . map read . lines <$> hGetContents stdin
 
 solve :: IS.IntSet -> [IS.Key]
-solve is = do
-  a <- IS.toAscList is
-  b <- IS.toAscList is
-  guard $ a < b
-  let c = 2020 - (a + b)
-  guard $ b < c
-  guard $ IS.member c is
+solve is = do             -- list monad
+  a <- IS.toAscList is    -- take a from the sorted set of inputs
+  b <- IS.toAscList is    -- take b from the sorted set of inputs
+  let c = 2020 - (a + b)  -- Instead of picking c and testing
+                          -- a + b + c == 2020, we calculate what
+                          -- c is needed to make the test work.
+  guard $ a < b && b < c  -- To prevent results which are simply
+                          -- permutations of a, b, c we constrain
+                          -- the ordering so only one is found.
+  guard $ IS.member c is  -- Test if c is present in our input.
+                          -- This is faster than linearly stepping
+                          -- through the input set and testing
+                          -- the summed values are 2020.
   return $ a * b * c
 
 main :: IO ()
