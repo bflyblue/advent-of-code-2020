ff5bfb8b922c5fe8aad0b929b843fa9f34dab8ce
Shaun Sharples <shaun.sharples@gmail.com>
2020-12-13 11:46:01 +0200

    day3

--- day3/Main.hs
+++ day3/Main.hs
@@ -5,12 +5,9 @@ module Main where
 
 import Control.Applicative (empty)
 import Control.Monad (void)
-import Data.Massiv.Array (Array, S, Ix2(..), Sz(..), Sz2(..))
+import Data.Massiv.Array (Array, B, Ix2 (..), Sz (..), Sz2 (..))
 import qualified Data.Massiv.Array as A
 import Data.Void (Void)
-import Foreign.Ptr
-import Foreign.Storable
-import System.IO (hGetContents, stdin)
 import Text.Megaparsec ((<|>))
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Char as C
@@ -24,8 +21,8 @@ import qualified Text.Megaparsec.Char.Lexer as L
 -- Using the `massiv` package for the first time to see how it works, instead of
 -- using [[Square]]. Using the Storable rather than Boxed type.
 data Map = Map
-  { mapArray :: Array S Ix2 Square
-  , mapSize :: Sz2
+  { mapArray :: Array B Ix2 Square,
+    mapSize :: Sz2
   }
   deriving (Show)
 
@@ -33,18 +30,11 @@ data Map = Map
 data Square = Open | Tree
   deriving (Show, Eq, Ord, Enum)
 
--- Storable instance using the enum Int value
-instance Storable Square where
-  sizeOf _ = sizeOf @Int 0
-  alignment _ = alignment @Int 0
-  peek ptr = toEnum <$> peek (castPtr ptr)
-  poke ptr a = poke (castPtr ptr) (fromEnum a)
-
 -- read the square at the given coordinate, taking right wrapping into account
-index :: Map -> (Int, Int) -> Square
+index :: Map -> (Int, Int) -> Maybe Square
 index m (x, y) =
   let (Sz2 _ width) = mapSize m
-  in A.index' (mapArray m) (y :. x `mod` width)
+   in A.index (mapArray m) (y :. x `mod` width)
 
 {-
     We use a small parser to read each line of input
@@ -66,29 +56,50 @@ line = M.some square <* (void C.eol <|> M.eof)
 
 -- parse an single square
 square :: Parser Square
-square = M.choice
-  [ Open <$ C.char '.'
-  , Tree <$ C.char '#'
-  ]
+square =
+  M.choice
+    [ Open <$ C.char '.',
+      Tree <$ C.char '#'
+    ]
 
+-- parse map
 map' :: Parser Map
 map' = do
-    ls <- M.many line <* M.eof
-    let a = A.fromLists' A.Seq ls
-    return Map { mapArray = a , mapSize = A.size a }
+  ls <- M.many line <* M.eof
+  let a = A.fromLists' A.Seq ls
+  return Map {mapArray = a, mapSize = A.size a}
 
 -- parse entire input from stdin, raising exception on failure
 input :: IO Map
 input = do
-  i <- hGetContents stdin
+  i <- getContents
   case M.parse map' "stdin" i of
     Left bundle -> error (M.errorBundlePretty bundle)
     Right m -> return m
 
-solve :: [[Square]] -> Int
-solve = undefined
+slopeEncounters :: (Int, Int) -> Map -> Int
+slopeEncounters (sx, sy) map = go (0, 0)
+  where
+    go pos@(x, y) =
+      let next = go (x + sx, y + sy)
+       in case index map pos of
+            Just Tree -> 1 + next
+            Just Open -> 0 + next
+            Nothing -> 0
+
+part1 :: Map -> Int
+part1 = slopeEncounters (3, 1)
+
+part2 :: Map -> Int
+part2 map =
+  slopeEncounters (1, 1) map
+    * slopeEncounters (3, 1) map
+    * slopeEncounters (5, 1) map
+    * slopeEncounters (7, 1) map
+    * slopeEncounters (1, 2) map
 
 main :: IO ()
 main = do
-  m <- input
-  print $ index m (35, 0)
+  map <- input
+  print $ part1 map
+  print $ part2 map
